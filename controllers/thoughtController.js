b50f778f84df78e95799330cfe0d44921ebd8c6e
Hiro Protagonist <shd327@gmail.com>
2022-12-22 17:13:19 -0700

    fix thoughts model for reaction schema by requring it, also added thought controller placeholder

---
+++ controllers/thoughtController.js
@@ -0,0 +1,25 @@
+const { Thoughts, User } = require("../models");
+
+module.exports = {
+    getThoughts(req, res) {
+
+    },
+    getThoughtbyId(req, res) {
+
+    },
+    createThought(req, res) {
+
+    },
+    updateThought(req, res) {
+
+    },
+    deleteThought(req, res) {
+
+    },
+    createReaction(req, res) {
+
+    },
+    deleteReaction(req, res) {
+
+    },
+};
