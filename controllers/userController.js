622f2991c3a5d12228248bf67f2b5fe81215a5de
Hiro Protagonist <shd327@gmail.com>
2022-12-22 17:08:46 -0700

    added userController functions placeholders

---
+++ controllers/userController.js
@@ -0,0 +1,32 @@
+const { User, Thoughts } = require("../models");
+
+module.exports = {
+    // Get all users
+    getUsers(req, res) {
+
+    },
+    // Get a single user
+    getSingleUser(req, res) {
+
+    },
+    // update a user
+    updateUser(req, res) {
+
+    },
+    // create a new user
+    createUser(req, res) {
+
+    },
+    // Delete a user and associated apps
+    deleteUser(req, res) {
+
+    },
+    // Add a friend
+    addFriends(req, res) {
+
+    },
+    // Remove a friend
+    removeFriends(req, res) {
+
+    },
+};
