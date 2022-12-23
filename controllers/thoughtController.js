d9f75c8abf1456596317622bd8366bfdf70b89a8
Hiro Protagonist <shd327@gmail.com>
2022-12-22 18:01:18 -0700

    added user and thought getAll and get by ID functions

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -8,6 +8,15 @@ module.exports = {
 
     },
     getThoughtbyId(req, res) {
+        Thought.findOne({ _id: req.params.thoughtId })
+            .select('-__v')
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: 'No thought with that ID' })
+                    : res.json(thought)
+            ).catch((err) => {
+                res.status(500).json(err);
+            })
 
     },
     createThought(req, res) {
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,24 @@
-const { User, Thoughts } = require("../models");
+const { User, Thought } = require("../models");
 
 module.exports = {
     // Get all users
     getUsers(req, res) {
+        User.find()
+            .then((users) => res.json(users))
+            .catch((err) => res.status(500).json(err))
 
     },
     // Get a single user
     getSingleUser(req, res) {
-
+        User.findOne({ _id: req.params.userId })
+            .select('-__v')
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No user with that ID' })
+                    : res.json(user)
+            ).catch((err) => {
+                res.status(500).json(err);
+            })
     },
     // update a user
     updateUser(req, res) {
